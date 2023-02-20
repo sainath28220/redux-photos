e34d86708512e4536a82321e923c9c346d041b11
sainath28220 <92886694+sainath28220@users.noreply.github.com>
2023-02-20 20:09:20 +0530

    5 files modified, 3 added

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,7 +1,51 @@
-const UsersList = () =>{
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers,addUser } from '../store';
+import Skeleton from './Skeleton';
+import Button from './Button';
+
+const UsersList = () => {
+  const dispatch = useDispatch();
+  const {
+    isLoading,
+    data,
+    error,
+  } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  }
+  if (isLoading) {
+    return <Skeleton times={1} className="h-10 w-full" />;
+  }
+  if (error) {
+    return <div>error fetching data </div>;
+  }
+  const renderedUsers = data.map((user)=>{
+    return(
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor-pointer'>
+          {user.name}
+        </div> 
+      </div>
+    )
+  });
+
   return(
-    <div>UsersList</div>
-  )
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className='m-2 text-xl'>Users</h1>
+        <Button onClick={handleUserAdd}>
+          +Add User
+        </Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UsersList;
