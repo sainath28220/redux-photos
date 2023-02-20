8de39f7256923ed8151db798486f96727a235cca
sainath28220 <92886694+sainath28220@users.noreply.github.com>
2023-02-20 23:23:35 +0530

    3 files modified

--- src/store/thunks/fetchUsers.jsx
+++ src/store/thunks/fetchUsers.jsx
@@ -2,7 +2,7 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const fetchUsers = createAsyncThunk('user/fetch',async()=>{
-  const response = await axios.get('http://localhost:3005/users');
+  const response = await axios.get('http://localhost:3001/users');
   return response.data;
 })
 
